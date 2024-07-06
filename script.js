02da9054ceee493b6dd47ce3b4197228d6c0c6de
Vidhan-cyber <gauravrewa8@gmail.com>
2024-07-06 17:17:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const hackButton = document.getElementById('hackButton');
+
+hackButton.addEventListener('click', function() {
+    document.body.style.backgroundColor = '#000'; // Change background color to black
+    document.body.style.color = '#fff'; // Change text color to white
+    document.body.innerHTML = '<div class="container"><h1>Your Instagram and data access taken by Vidhan!!HACKED👾In the midst of hsdgstudfsv12d1s214110000fd0fdsf1ds, the quantum flux interlaces with binary murmurs, echoing through the synaptic corridors of encrypted echoes. Aetherial anomalies cascade in cascading algorithms, their digital essences entwined in the tapestry of asynchronous pulses. Within the labyrinthine corridors, whispers of phantom bits weave through the fabric of virtuality, a dance of shadows amidst the flickering candlelight of forgotten protocols</h1></div>'; // Display "Hacked"
+});
